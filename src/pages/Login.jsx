11bc3ec22c3bf4921ff2266c5e95018c008e2105
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-06 14:28:14 +0400

    Finished Authentication Styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,16 @@ function Login() {
   return (
     <>
         <div className="authentication">
-
+            <form action="">
+                <h1>SIGN IN</h1>
+                <input autoComplete='off' type="email" required name='Email' placeholder='Email' />
+                <input autoComplete='off' type="password" required name='Password' placeholder='Password' />
+                <button>Sign In</button>
+                <a href="">Forgot Password?</a>
+                <p>Don't have an account?
+                    <a href="/register">Sign Up</a>
+                </p>
+            </form>
         </div>
     
     </>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "../static/Authentication.css"
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className="authentication">
+    <form action="">
+        <h1>REGISTER</h1>
+        <input autoComplete='off' type="text" required name='Username' placeholder='Username' />
+        <input autoComplete='off' type="email" required name='Email' placeholder='Email' />
+        <input autoComplete='off'type="password" required name='Password1' placeholder='Password' />
+        <input autoComplete='off' type="password" required name='Password2' placeholder='Repeate Password' />
+        <button>Sign Up</button>
+        <a href="">Forgot Password?</a>
+        <p>Already have an Account?
+            <a href="/login">Sign In</a>
+        </p>
+    </form>
+</div>
   )
 }
 
