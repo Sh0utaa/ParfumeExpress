71ab5f16de17b9d4badb25a20be8c799a4313ff7
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-10 11:53:29 +0400

    made carousel need to style it better

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import "../static/ImageContainer.css"
+import React, { useState } from 'react';
+import "../static/ImageContainer.css";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
 
 function ImageContainer() {
+  const images = [
+    "src/assets/ManModel.png",
+    "src/assets/WomanModel.png",
+    "src/assets/UnisexModel.png"
+  ];
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
   return (
-    <div className="image-container">
-    <div className="image-wrapper">
-      <img src="src/assets/ManModel.png" alt="manModelPicture" />
-      <div className="overlay"></div>
-    </div>
-    <div className="image-wrapper">
-      <img src="src/assets/WomanModel.png" alt="womanModelPicture" />
-      <div className="overlay"></div>
-    </div>
-    <div className="image-wrapper">
-      <img src="src/assets/UnisexModel.png" alt="unisexModelPicture" />
-      <div className="overlay"></div>
-    </div>
-  </div>
-  )
+    <>
+      <div className="image-container">
+        {images.map((image, index) => (
+          <div className="image-wrapper" key={index}>
+            <img src={image} alt={`modelPicture${index}`} />
+          </div>
+        ))}
+      </div>
+
+      <div className="carousel-container">
+        <button className="carousel-button left" onClick={handlePrev}>
+          <BsArrowLeftCircleFill />
+        </button>
+        <div className="carousel">
+          <div className="slide">
+            <img src={images[currentIndex]} alt={`modelPicture${currentIndex}`} />
+          </div>
+        </div>
+        <button className="carousel-button right" onClick={handleNext}>
+          <BsArrowRightCircleFill />
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default ImageContainer
+export default ImageContainer;
