2c6181890924da80a470a89da67870f27befaf79
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-12 09:56:08 +0400

    Added protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Register from './pages/Register'
 import { AuthProvider } from './utils/AuthContext';
 import Account from './pages/Account'
+import ProtectedRoutes from './utils/ProtectedRoutes'
 
 function App() {
 
@@ -19,8 +20,10 @@ function App() {
             <Route path="/home" element={<Home />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/register" element={<Register />}></Route>
-            <Route path='/account' element={<Account />}></Route>
 
+            <Route element={<ProtectedRoutes/>}>
+              <Route path='/account' element={<Account />}></Route>
+            </Route>
 
           </Routes>
         </AuthProvider>
