9650852f9951754dbf6d315de7d8235731ec5283
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-18 23:01:58 +0400

    fixed some small styling things

--- src/pages/Filter.jsx
+++ src/pages/Filter.jsx
@@ -14,9 +14,13 @@ const Filter = ({ isFilterbarActive, toggleFilterbar }) => {
         <div className={`filter-sidebar ${isFilterbarActive ? "active" : ""}`}>
           
 
-          <button onClick={toggleFilterbar}>Toggle</button>
+          <button onClick={toggleFilterbar}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+          </button>
 
-          <FilterSlider></FilterSlider>
+          <div className="filter-sidebar-styling">
+            <FilterSlider></FilterSlider>
+          </div>
 
             
           </div>
