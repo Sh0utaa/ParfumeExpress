e4ff807cbc71490987e1f43d770b8662acef4822
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-19 17:40:38 +0400

    moved filing structure for Vercel

--- src/components/LogoSlider.jsx
+++ src/components/LogoSlider.jsx
@@ -5,27 +5,29 @@ function LogoSlider() {
     
     <div className="logos-container">
     <div className="logo-slider">
-      <img src="https://cloud.appwrite.io/v1/storage/buckets/673c9114001a74aba820/files/673c91420018c7738530/view?project=67179e18003ccf5af8e5&project=67179e18003ccf5af8e5&mode=admin" alt="" />
-      <img src="https://cloud.appwrite.io/v1/storage/buckets/673c9114001a74aba820/files/673c914b002a4d881c3d/view?project=67179e18003ccf5af8e5&project=67179e18003ccf5af8e5&mode=admin" alt="" />
-      <img src="src\logos\chanel-1.svg" alt="" />
-      <img src="src\logos\dolce-gabbana-1.svg" alt="" />
-      <img src="https://cloud.appwrite.io/v1/storage/buckets/673c9114001a74aba820/files/673c913100015ed5ccfe/view?project=67179e18003ccf5af8e5&project=67179e18003ccf5af8e5&mode=admin" alt="" />
-      <img src="src\logos\gucci.svg" alt="" />
-      <img src="src\logos\jean-paul-gaultier.svg" alt="" />
-      <img src="src\logos\versace.svg" alt="" />
-      <img src="src\logos\yves-saint-laurent.svg" alt="" />
+      <img src="src\svg\logos\armani.svg" alt="" />
+      <img src="src\svg\logos\boss-hugo-boss.svg" alt="" />
+      <img src="src\svg\logos\calvin-klein-1.svg" alt="" />
+      <img src="src\svg\logos\chanel-1.svg" alt="" />
+      <img src="src\svg\logos\dolce-gabbana-1.svg" alt="" />
+      <img src="src\svg\logos\giorgio-armani.svg" alt="" />
+      <img src="src\svg\logos\gucci.svg" alt="" />
+      <img src="src\svg\logos\jean-paul-gaultier.svg" alt="" />
+      <img src="src\svg\logos\versace.svg" alt="" />
+      <img src="src\svg\logos\yves-saint-laurent.svg" alt="" />
     </div>
 
     <div className="logo-slider">
-      <img src="https://cloud.appwrite.io/v1/storage/buckets/673c9114001a74aba820/files/673c91420018c7738530/view?project=67179e18003ccf5af8e5&project=67179e18003ccf5af8e5&mode=admin" alt="" />
-      <img src="https://cloud.appwrite.io/v1/storage/buckets/673c9114001a74aba820/files/673c914b002a4d881c3d/view?project=67179e18003ccf5af8e5&project=67179e18003ccf5af8e5&mode=admin" alt="" />
-      <img src="src\logos\chanel-1.svg" alt="" />
-      <img src="src\logos\dolce-gabbana-1.svg" alt="" />
-      <img src="https://cloud.appwrite.io/v1/storage/buckets/673c9114001a74aba820/files/673c913100015ed5ccfe/view?project=67179e18003ccf5af8e5&project=67179e18003ccf5af8e5&mode=admin" alt="" />
-      <img src="src\logos\gucci.svg" alt="" />
-      <img src="src\logos\jean-paul-gaultier.svg" alt="" />
-      <img src="src\logos\versace.svg" alt="" />
-      <img src="src\logos\yves-saint-laurent.svg" alt="" />
+      <img src="src\svg\logos\armani.svg" alt="" />
+      <img src="src\svg\logos\boss-hugo-boss.svg" alt="" />
+      <img src="src\svg\logos\calvin-klein-1.svg" alt="" />
+      <img src="src\svg\logos\chanel-1.svg" alt="" />
+      <img src="src\svg\logos\dolce-gabbana-1.svg" alt="" />
+      <img src="src\svg\logos\giorgio-armani.svg" alt="" />
+      <img src="src\svg\logos\gucci.svg" alt="" />
+      <img src="src\svg\logos\jean-paul-gaultier.svg" alt="" />
+      <img src="src\svg\logos\versace.svg" alt="" />
+      <img src="src\svg\logos\yves-saint-laurent.svg" alt="" />
     </div>
 </div> 
   )
