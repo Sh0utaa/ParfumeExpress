e87aca68a8d59bd6ba9c7b81a58306842a4cc743
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-20 13:32:04 +0400

    fixed contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,52 +1,59 @@
 import React from 'react'
 import "../static/Contact.css"
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTiktok } from "react-icons/fa";
 
 function Contact() {
   return (
     <>
         <div className="contact-container">
 
-            <div className="contact">
-                <div className="email">
-                    <h3>Send us a message!</h3>
-                    <input type="text" placeholder='Write something' />
-                    <button>SUBMIT</button>
-                </div>
-                <div className="referrals">
-                    <ul>
-                        <li>
-                            hi@gmail.com
-                        </li>
-                        <li>example@gmail.com</li>
-                        <li>+995 999 999 999</li>
-                    </ul>
+                <div className="contact">
+                    <div className="email">
+                        <h3>Send us a message!</h3>
+                        <div>
+                            <input type="text" placeholder='Write something' />
+                        </div>
+                        <button>SUBMIT</button>
+                    </div>
+                    <div className="referrals">
+                        <ul>
+                            <li>
+                                hi@gmail.com
+                            </li>
+                            <li>example@gmail.com</li>
+                            <li>+995 999 999 999</li>
+                        </ul>
 
-                    <h3>Like what you see? This website was made by <a href="/">ShotaTevdorashvili</a>!</h3>
+                        <h3>Like what you see? This website was made by <a href="/">ShotaTevdorashvili</a>!</h3>
+                    </div>
                 </div>
-            </div>
 
-            <div className="socials-logos-container">
+                <div className="socials-logos-container">
                 <ul>
                     <li>
-                        FACEBOOK
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FaFacebookF />
+                    </a>
                     </li>
                     <li>
-                        FACEBOOK
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram />
+                    </a>
                     </li>
                     <li>
-                        FACEBOOK
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedinIn />
+                    </a>
                     </li>
                     <li>
-                        FACEBOOK
-                    </li>
-                    <li>
-                        FACEBOOK
-                    </li>
-                    <li>
-                        FACEBOOK
+                    <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+                        <FaTiktok />
+                    </a>
                     </li>
                 </ul>
-            </div>
+                </div>
+
+                <div className="bootstrap-box"></div>
             
 
         </div>
